626d1618bb0f39845d276f0704ca70776326ee74
efraintinoco <oakcliff_21@yahoo.com>
2022-03-30 08:11:09 -0500

    change syntax

--- exercises/2/index.js
+++ exercises/2/index.js
@@ -1,2 +1,2 @@
-const p = document.querySelector ("p")
+const p = document.querySelector("p")
 p.textContent = "Two types of corgi lords Pembroke and Cardigan"
--- exercises/7/index.js
+++ exercises/7/index.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement ("h2")
+const h2 = document.createElement("h2")
 h2.textContent = "Favorite Corgis"
 const user = document.querySelector(".user")
 user.append(h2)
